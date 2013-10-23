9ec2f3b4c2343b89be434dc9cca817c128ef6c43
revir <revir.qing@gmail.com>
2013-10-23 08:47:39 +0800

    update, but not finished

--- myclock/clockManager.js
+++ myclock/clockManager.js
@@ -9,12 +9,14 @@ ClockManager.alertType = {
 };
 
 ClockManager.defaultClock = {
-    id:"defaultClock",
+    id:"Default Clock",
     name:"Default Clock",
     description:"Default clock",
     time: "2013-10-21 08:00:00",
     repeat:["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-    alert: ClockManager.alertType.alert,
+    alert: ClockManager.alertType.alert
+    //countDown
+    //createTime
 };
 
 ClockManager.init = function () {
@@ -29,3 +31,45 @@ ClockManager.loadClocks = function() {
 
     ClockManager.enabled = Settings.getValue("enableClocks", true);
 };
+
+ClockManager.save = function saveClocks() {
+    Settings.setObject("clocks", ClockManager.clocks);
+    Settings.setValue("enableClocks", ClockManager.enabled);
+};
+
+ClockManager.isEnabled = function isEnabled() {
+    return ClockManager.enabled;
+};
+
+ClockManager.setEnabled = function setEnabled(enabled) {
+    ClockManager.enabled = !!enabled;
+};
+
+ClockManager.getClocks = function getClocks() {
+    var arr = [];
+    for (var i in ClockManager.clocks) {
+        if (clocks.hasOwnProperty(i)) {
+            arr[arr.length] = clocks[i];
+        }
+    }
+    return arr;
+};
+
+ClockManager.setClocks = function setclocks(clocks) {
+    t = $.extend(true, {}, clocks);
+    ClockManager.clocks = t;
+};
+
+ClockManager.addClock = function addClock(clock) {
+    ClockManager.clocks[clock.id] = clock;
+    ClockManager.save();
+    //?? apply?
+};
+
+ClockManager.getSortedClocks = function getSortedClocks() {
+    var arr = ClockManager.getClocks();
+    arr = arr.sort(Utils.compareNamedObjects);
+    return arr;
+};
+
+ClockManager.init();
