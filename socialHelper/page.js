f3847e97fbd8d444993143a77ff0b6556695c380
revir <revir.qing@gmail.com>
2013-11-13 20:44:47 +0800

    update

--- socialHelper/page.js
+++ socialHelper/page.js
@@ -1,1 +1,18 @@
-alert('test');
+jQuery(document).ready(function() {
+	if (location.protocol === 'file:') {
+		var pNode = jQuery('#social_helper_div .social_helper_p');
+		if (!pNode.length) {
+			alert('cannot find the content node!');
+			return;
+		}
+		var data = {};
+		data.type = 'postToSocial';
+		data.content = pNode.text();
+		console.log(content);
+		if(pNode.hasClass('social_weibo'))
+			data.weibo =  true;
+
+		chrome.runtime.sendMessage(data);
+		// window.close();
+	}
+});
