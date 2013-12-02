162d2218ea3244b8816d26829339157be4221def
revir <revir.qing@gmail.com>
2013-12-02 18:01:13 +0800

    update

--- socialHelper/core.js
+++ socialHelper/core.js
@@ -22,38 +22,40 @@ var _post_data = {};
 console.info('[temp] core init...');
 
 chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		if (request.type === 'postToSocial') {
-			_post_data = jQuery.extend(true, {}, request);
-			setTimeout(function() {
-				console.info('timeout: _post_data will be cleared');
-				_post_data = {};
-			}, request.keepTime);
+    function(request, sender, sendResponse) {
+        if (request.type === 'postToSocial') {
+            _post_data = jQuery.extend(true, {}, request);
+            setTimeout(function() {
+                console.info('timeout: _post_data will be cleared');
+                _post_data = {};
+            }, request.keepTime);
 
-			jQuery.each(_post_data.socialWebSite, function(index, url) {
-				chrome.tabs.create({
-					url: url
-				});
-			});
-			sendResponse('good');
-		} else if (request.type === 'getInfo') {
-			console.info('getInfo event: '+request.url);
-			var t = jQuery.extend(true, {}, _post_data);
-			sendResponse(t);
-		} else if (request.type === 'postDone') {
-			if (request.url) {
-				console.info('postDone event: ' + request.url);
-				if (!_post_data.socialWebSite) {
-					console.info('but the _post_data is already empty.');
-					return;
-				}
-				var i = _post_data.socialWebSite.indexOf(request.url);
-				if (i !== -1) {
-					_post_data.socialWebSite.splice(i, 1);
-				}
-				if (_post_data.socialWebSite.length === 0) {
-					_post_data = {};
-				}
-			}
-		}
-	});
+            _post_data.socialWebSite.forEach(function(site) {
+                chrome.tabs.create({
+                    url: site
+                });
+            });
+            if(_post_data.closeTemp)
+                chrome.tabs.remove(sender.tab.id);
+        } else if (request.type === 'getInfo') {
+            console.info('getInfo event: ' + request.url);
+            if (_post_data.socialWebSite && _post_data.socialWebSite.indexOf(request.url) !== -1) {
+                sendResponse(_post_data);
+            }
+        } else if (request.type === 'postDone') {
+            if (request.url) {
+                console.info('postDone event: ' + request.url);
+                if (!_post_data.socialWebSite) {
+                    console.info('but the _post_data is already empty.');
+                    return;
+                }
+                var i = _post_data.socialWebSite.indexOf(request.url);
+                if (i !== -1) {
+                    _post_data.socialWebSite.splice(i, 1);
+                }
+                if (_post_data.socialWebSite.length === 0) {
+                    _post_data = {};
+                }
+            }
+        }
+    });
--- socialHelper/page.js
+++ socialHelper/page.js
@@ -24,172 +24,168 @@ gsite.twitter_host = "https://twitter.com";
 gsite.googleplus_host = 'https://plus.google.com';
 
 function processFile() {
-	var pNode = jQuery('#social_helper_div .social_helper_p');
-	if (!pNode.length) {
-		alert('cannot find the content node!');
-		return;
-	}
-	var data = {};
-	data.type = 'postToSocial';
-	data.content = pNode.text();
-	data.keepTime = 5 * 60 * 1000;
-	data.socialWebSite = [];
-	console.log(data.content);
-	if (pNode.hasClass('weibo')) {
-		data.socialWebSite[data.socialWebSite.length] = gsite.weibo_host;
-	}
-	if (pNode.hasClass('twitter')) {
-		data.socialWebSite[data.socialWebSite.length] = gsite.twitter_host;
-	}
-	if (pNode.hasClass('googleplus')) {
-		data.socialWebSite[data.socialWebSite.length] = gsite.googleplus_host;
-	}
-
-	chrome.runtime.sendMessage(data, function(msg){
-		window.close();
-	});
+    var pNode = jQuery('#social_helper_div .social_helper_p');
+    if (!pNode.length) {
+        alert('cannot find the content node!');
+        return;
+    }
+    var data = {};
+    data.type = 'postToSocial';
+    data.content = pNode.text();
+    data.keepTime = 5 * 60 * 1000;
+    data.socialWebSite = [];
+    data.noProcess = false;
+    data.closeTemp = true;
+    console.log(data.content);
+    if (pNode.hasClass('weibo')) {
+        data.socialWebSite[data.socialWebSite.length] = gsite.weibo_host;
+    }
+    if (pNode.hasClass('twitter')) {
+        data.socialWebSite[data.socialWebSite.length] = gsite.twitter_host;
+    }
+    if (pNode.hasClass('googleplus')) {
+        data.socialWebSite[data.socialWebSite.length] = gsite.googleplus_host;
+    }
+    chrome.runtime.sendMessage(data);
 }
 
 function processWeibo(data) {
-	var checkWeiboLogin = function(data) {
-		if (!/\/u\//.test(location.pathname)) {
-			alert('you didn\'t login weibo yet, please login in ' + data.keepTime / (60 * 1000) + ' minutes, otherwise it will be timeout.');
-			return false;
-		}
-		return true;
-	};
-	waitForAjax('textarea.input_detail', '', function(success) {
-		if (!success) {
-			if (checkWeiboLogin(data)) {
-				alert('Cannot find the input area on weibo');
-				return;
-			}
-			return;
-		}
-		var textArea = jQuery('textarea.input_detail'),
-			sendBtn = jQuery('a.send_btn');
-		if (!textArea.length || !sendBtn.length) {
-			alert('Cannot find the input area or send button on weibo');
-			return;
-		}
-		textArea[0].focus();
-		textArea[0].value = data.content;
-		autoKeypress(textArea[0]);
+    var checkWeiboLogin = function(data) {
+        if (!/\/u\//.test(location.pathname)) {
+            alert('you didn\'t login weibo yet, please login in ' + data.keepTime / (60 * 1000) + ' minutes, otherwise it will be timeout.');
+            return false;
+        }
+        return true;
+    };
+    waitForAjax('textarea.input_detail', '', function(success) {
+        if (!success) {
+            if (checkWeiboLogin(data)) {
+                alert('Cannot find the input area on weibo');
+                return;
+            }
+            return;
+        }
+        var textArea = jQuery('textarea.input_detail'),
+            sendBtn = jQuery('a.send_btn');
+        if (!textArea.length || !sendBtn.length) {
+            alert('Cannot find the input area or send button on weibo');
+            return;
+        }
+        textArea[0].focus();
+        textArea[0].value = data.content;
+        autoKeypress(textArea[0]);
 
-		setTimeout(function(){
-			sendBtn = jQuery('a.send_btn');
-			sendBtn[0].click();
-			console.info('post to weibo successfully!');
-			chrome.runtime.sendMessage({
-				type: 'postDone',
-				url: location.origin
-			});
-		}, 2000);
-	});
+        setTimeout(function() {
+            sendBtn = jQuery('a.send_btn');
+            sendBtn[0].click();
+            console.info('post to weibo successfully!');
+            chrome.runtime.sendMessage({
+                type: 'postDone',
+                url: location.origin
+            });
+        }, 2000);
+    });
 
 }
 
 function processTwitter(data) {
-	var checkLogin = function() {
-		if (jQuery('#signin-link').length) {
-			alert('you didn\'t login twitter yet, please login in ' + data.keepTime / (60 * 1000) + ' minutes, otherwise it will be timeout.');
-			return false;
-		}
-		return true;
-	};
+    var checkLogin = function() {
+        if (jQuery('#signin-link').length) {
+            alert('you didn\'t login twitter yet, please login in ' + data.keepTime / (60 * 1000) + ' minutes, otherwise it will be timeout.');
+            return false;
+        }
+        return true;
+    };
 
-	waitForAjax('#tweet-box-mini-home-profile', '', function(success) {
-		if (!success) {
-			if (checkLogin()) {
-				alert('Cannot find the input area on twitter');
-				return;
-			}
-			return;
-		}
-		var node = jQuery('#tweet-box-mini-home-profile');
-		var tbtn = jQuery('div.home-tweet-box button.js-tweet-btn');
-		if (!node.length || !tbtn.length) {
-			alert('Cannot find the input area or send button on twitter');
-			return;
-		}
-		node[0].focus();
-		node[0].innerText = data.content;
+    waitForAjax('#tweet-box-mini-home-profile', '', function(success) {
+        if (!success) {
+            if (checkLogin()) {
+                alert('Cannot find the input area on twitter');
+                return;
+            }
+            return;
+        }
+        var node = jQuery('#tweet-box-mini-home-profile');
+        var tbtn = jQuery('div.home-tweet-box button.js-tweet-btn');
+        if (!node.length || !tbtn.length) {
+            alert('Cannot find the input area or send button on twitter');
+            return;
+        }
+        node[0].focus();
+        node[0].innerText = data.content;
 
-		setTimeout(function() {
-			var tbtn = jQuery('div.home-tweet-box button.js-tweet-btn');
-			tbtn[0].click();
-			console.info('post to twitter successfully!');
-			chrome.runtime.sendMessage({
-				type: 'postDone',
-				url: location.origin
-			});
-		}, 1000);
-	});
+        setTimeout(function() {
+            var tbtn = jQuery('div.home-tweet-box button.js-tweet-btn');
+            tbtn[0].click();
+            console.info('post to twitter successfully!');
+            chrome.runtime.sendMessage({
+                type: 'postDone',
+                url: location.origin
+            });
+        }, 1000);
+    });
 }
 
 function processGooglePlus(data) {
-	var checkLogin = function() {
-		if (/account/.test(location.origin)) {
-			alert('you didn\'t login google yet, please login in ' + data.keepTime / (60 * 1000) + ' minutes, otherwise it will be timeout.');
-			return false;
-		}
-		return true;
-	};
+    var checkLogin = function() {
+        if (/account/.test(location.origin)) {
+            alert('you didn\'t login google yet, please login in ' + data.keepTime / (60 * 1000) + ' minutes, otherwise it will be timeout.');
+            return false;
+        }
+        return true;
+    };
 
-	waitForAjax('div[guidedhelpid="sharebox_textarea"]', '', function(success) {
-		if (!success) {
-			if (checkLogin()) {
-				alert('Cannot find the input area on google plus');
-				return;
-			}
-			return;
-		}
-		var node = jQuery('div[guidedhelpid="sharebox_textarea"]');
-		node[0].click();
-		setTimeout(function() {
-			var textArea = jQuery('div[g_editable="true"]');
-			var btn = jQuery('div[guidedhelpid="sharebutton"]');
-			if (!textArea.length || !btn.length) {
-				alert('Cannot find the input area or send button on twitter');
-				return;
-			}
-			autoKeypress(textArea[0]);
-			textArea[0].innerText = data.content;
-			setTimeout(function() {
-				btn[0].click();
+    waitForAjax('div[guidedhelpid="sharebox_textarea"]', '', function(success) {
+        if (!success) {
+            if (checkLogin()) {
+                alert('Cannot find the input area on google plus');
+                return;
+            }
+            return;
+        }
+        var node = jQuery('div[guidedhelpid="sharebox_textarea"]');
+        node[0].click();
+        setTimeout(function() {
+            var textArea = jQuery('div[g_editable="true"]');
+            var btn = jQuery('div[guidedhelpid="sharebutton"]');
+            if (!textArea.length || !btn.length) {
+                alert('Cannot find the input area or send button on google plus');
+                return;
+            }
+            autoKeypress(textArea[0]);
+            textArea[0].innerHTML = data.content;
+            setTimeout(function() {
+                btn[0].click();
 
-				console.info('post to google plus successfully!');
-				chrome.runtime.sendMessage({
-					type: 'postDone',
-					url: location.origin
-				});
-			}, 1000);
+                console.info('post to google plus successfully!');
+                chrome.runtime.sendMessage({
+                    type: 'postDone',
+                    url: location.origin
+                });
+            }, 1000);
 
-		}, 1000);
-	});
+        }, 1000);
+    });
 }
 
 jQuery(document).ready(function() {
-	if (location.protocol === 'file:') {
-		processFile();
-	} else {
-		chrome.runtime.sendMessage({
-			type: 'getInfo',
-			url: location.origin
-		}, function(response) {
-			if (response && response.socialWebSite && response.socialWebSite.length) {
-				if (response.socialWebSite.indexOf(location.origin) !== -1) {
-					console.log('origin: ' + location.origin);
-					if (location.origin === gsite.weibo_host) {
-						return processWeibo(response);
-					} else if (location.origin === gsite.twitter_host) {
-						return processTwitter(response);
-					} else if (location.origin === gsite.googleplus_host) {
-						return processGooglePlus(response);
-					}
-				}
-			}
-			console.log('nothing, url: '+location.origin);
-		});
-	}
+    if (location.protocol === 'file:') {
+        processFile();
+    } else {
+        chrome.runtime.sendMessage({
+            type: 'getInfo',
+            url: location.origin
+        }, function(response) {
+            console.log('origin: ' + location.origin);
+            if(response.noProcess)
+                return;
+            if (location.origin === gsite.weibo_host) {
+                processWeibo(response);
+            } else if (location.origin === gsite.twitter_host) {
+                processTwitter(response);
+            } else if (location.origin === gsite.googleplus_host) {
+                processGooglePlus(response);
+            }
+        });
+    }
 });
